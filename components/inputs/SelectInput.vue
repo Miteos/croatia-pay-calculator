e3145ec2c1d8fa5a709774ed8bba0fa6526d690f
Matej <matej.kapular@gauss.hr>
2021-01-26 01:25:53 +0100

    Initial commit

---
+++ components/inputs/SelectInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="custom-select">
+    <label for="select">
+      {{ label }}
+    </label>
+    <select id="select" :value="chosenValue">
+      <option v-for="option in options" :key="option" :value="chosenValue">
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    chosenValue: {
+      type: String,
+      default: 'Click to choose...',
+    },
+  },
+  data() {
+    return {}
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <SelectInput label="whatever" :options="testArray" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      testArray: ['testerino', 'fuck you all'],
+    }
+  },
+}
+</script>
+
+<style></style>
